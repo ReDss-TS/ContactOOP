c4052959257d18a3788428d4a4dc2f8a7ee0d189
Taras Serhiienko <taras.serhiienko@gmail.com>
2017-10-10 22:42:22 +0300

    Sanitize func with recursion

--- classes/Filters.php
+++ classes/Filters.php
@@ -1,44 +1,22 @@
 <?php
 
-class Filters//TODO recursion!
+class Filters
 {   
-    // public function sanitizeSpecialCharsInMultiArrays($data)
-    // {
-
-    //     if (!empty($data)) {
-    //         foreach ($data as $key => $value) {
-    //             foreach ($value as $k => $val) {
-    //                 $resultVal[$k] = filter_var($val, FILTER_SANITIZE_SPECIAL_CHARS);
-    //                 $filteredResult[$key] = $resultVal;
-    //             }
-    //         }
-    //     }
-    //     if (!empty($filteredResult)) {
-    //         return $filteredResult;
-    //     }
-    // }
-
-    // public function sanitizeSpecialChars($data)
-    // {
-    //     foreach ($data as $key => $value) {
-    //         $filteredResult[$key] = filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
-    //     }
-    //     if (!empty($filteredResult)) {
-    //         return $filteredResult;
-    //     }
-    // }
-
+    //input data: array
+    //return: array 
     public function sanitizeSpecialChars($data)
     {
+        $filteredResult = [];
+        $keys = [];
         if (!empty($data)) {
             foreach ($data as $key => $value) {
-                if (is_array($value)) {$this->sanitizeSpecialChars($value);}
-                echo '<pre>';
-                var_dump($value);
-                echo '</pre>';
-                $filteredResult[$key] = filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
+                if (is_array($value)) {
+                    $result = $this->sanitizeSpecialChars($value); 
+                    $filteredResult[$key] = $result;
+                } else {
+                    $filteredResult[$key] = filter_var($value, FILTER_SANITIZE_SPECIAL_CHARS);
+                }
             }
-            var_dump($filteredResult);
             return $filteredResult;
         }
     }
